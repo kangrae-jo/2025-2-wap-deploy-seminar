9b52fc28944e06d24d7ca27d8442911e21dfb2d6
kangrae <kangrae0501@gmail.com>
2025-09-26 18:47:40 +0900

    feat: home 루트로 접근 시 배포 성공 안내 페이지 표시

---
+++ src/main/java/deploy/HomeController.java
@@ -0,0 +1,16 @@
+package deploy;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("message", "배포가 완료되었습니다!!");
+        return "home";
+    }
+
+}
